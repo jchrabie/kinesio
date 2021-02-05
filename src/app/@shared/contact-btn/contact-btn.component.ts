fa5e9e0eefd7436b2065152ca0d59fcba6004a92
CHRABIE <joel.chrabie@cbp-group.com>
2021-02-05 23:31:33 +0100

    change secondary color optional for contact btn

--- src/app/@shared/contact-btn/contact-btn.component.ts
+++ src/app/@shared/contact-btn/contact-btn.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contact-btn',
   templateUrl: './contact-btn.component.html',
   styleUrls: ['./contact-btn.component.scss'],
 })
-export class ContactBtnComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class ContactBtnComponent {
+  @Input() secondary = false;
 }
